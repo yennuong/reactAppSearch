99534c1743ac417fb0047040197cae4473a8c849
Nguyen Thi Yen Nuong <nuong.nguyen@finviet.corp>
2019-08-21 14:31:28 +0700

    change Title

--- src/components/Title.js
+++ src/components/Title.js
@@ -12,7 +12,7 @@ class Title extends Component {
     render() {
         return (
             <div className="page-header">
-                <h1>Project 01 - ToDo List <small>ReactJS</small></h1>
+                <h1>01<small>ReactJS</small></h1>
             </div>
         );
     }
